5c74c521f2d495dd15a89a53db9c82a20be1666a
vkukh <kukhar.vitalii@gmail.com>
2023-08-24 21:42:45 +0300

    add inputs&add&remove

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TimerComponent } from './timer/timer.component';
@@ -30,6 +32,8 @@ import { PhaseComponent } from './timer/phase/phase.component';
     MatButtonModule,
     MatIconModule,
     MatSlideToggleModule,
+    MatInputModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/timer/control/control.component.ts
+++ src/app/timer/control/control.component.ts
@@ -14,7 +14,10 @@ export class ControlComponent {
   public isReplayEnable: boolean = false;
 
   public countUpColor: ThemePalette = 'primary';
-  public isCountUp: boolean = false;
+  public isDisableControl: boolean = false;
+  public roundCount: string = '01';
+  public warmUpCount: string = '01';
+  public phaseCount: string = '01';
 
   @Output() changeTimerState = new EventEmitter<States>();
 
@@ -26,12 +29,12 @@ export class ControlComponent {
       console.log(this.isPlayActive);
     } else {
       this.changeTimerState.emit(States.Play);
-      this.isCountUp = true;
+      this.isDisableControl = true;
     }
   }
   public onReplay(): void {
     this.changeTimerState.emit(States.RePlay);
-    this.isCountUp = false;
+    this.isDisableControl = false;
   }
   public onCountUp(event: MatSlideToggleChange): void {
     const { checked } = event;
@@ -39,6 +42,23 @@ export class ControlComponent {
       console.log('countUp');
     } else {
       console.log('countDown');
+    }
+  }
+
+  public onInputChange(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    const numValue = parseInt(inputElement.value, 10);
+  
+    if (numValue >= 1 && numValue <= 99) {
+      inputElement.value = numValue < 10 ? `0${numValue}` : `${numValue}`;
+    } else {
+      inputElement.value = '01';
+    }
+  }
+  public onKeyPress(event: KeyboardEvent): void {
+    const allowedKeys = /^[0-9]$/;
+    if (!event.key.match(allowedKeys)) {
+      event.preventDefault();
     }
   }
 }
